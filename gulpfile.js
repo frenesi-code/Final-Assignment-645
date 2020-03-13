10b6e9469a9185291ad21c45d8296d8bafb93b22
= <=>
2020-03-12 19:45:27 -0700

    improving the code folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,54 @@
-const { src, dest, watch } = require('gulp');
-var sass = require('gulp-sass');
-sass.compiler = require('node-sass');
 
-function css() {
-    return src('src/*.scss')
-        .pipe(sass())
-        .pipe(dest('dist/css'))
-}
+'use strict';
 
-exports.default = function () {
-    watch('src/*.scss', css);
-    watch('src/modules/*.scss', css);
-}
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
 
-//exports.default = css
+const baseDir = 'app';
+const sassFiles = 'app/scss/**/*.scss';
+const imageFiles = 'app/images/*';
+
+const cssDestination = 'dist/css';
+const imagesDestination = 'dist/images';
+
+const cssToDelete = 'dist/css/*.css';
+
+gulp.task('clean', () => {
+    return del([
+        cssToDelete,
+    ]);
+});
+
+gulp.task('sass', function () {
+    return gulp.src(sassFiles)
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest(cssDestination))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('images', function () {
+    return gulp.src(imageFiles)
+        .pipe(imagemin())
+        .pipe(gulp.dest(imagesDestination));
+});
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    })
+})
+
+gulp.task('watch', function () {    
+    gulp.watch(sassFiles, gulp.series('sass'));
+    gulp.watch(baseDir, gulp.series('browserSync'));    
+    gulp.watch(imageFiles, gulp.series('images'));
+});
+
+gulp.task('default', gulp.series(['clean', 'sass', 'images', 'browserSync', 'watch']));
